5067f52c4379c8c9df661f2254c9be01313348b1
adicomdotir <adicom.ir@gmail.com>
2017-03-30 09:59:24 +0430

    Add triangle class

--- RSATM.java
+++ RSATM.java
@@ -5,7 +5,8 @@ public class RSATM {
 		Shape[] shapes = {
 			new Rectangle(3,2),
 			new Square(3),
-			new Circle(3)
+			new Circle(3),
+			new Triangle(2,3,4,5)
 		};
 		for(Shape s : shapes) {
 			System.out.println("" + s);
---
+++ Triangle.java
@@ -0,0 +1,29 @@
+public class Triangle extends Shape {
+	private float base;
+	private float height;
+	private float side1;
+	private float side2;
+	
+	public Triangle() {}
+	public Triangle(float b, float h, float s1, float s2) {
+		this.base = b;
+		this.height = h;
+		this.side1 = s1;
+		this.side2 = s2;
+	}
+	
+	public void setBase(float b) { this.base = b; }
+	public float getBase() { return base; }
+	public void setHeight(float h) { this.height = h; }
+	public float getHeight() { return height; }
+	
+	@Override
+	public float getArea() {
+		return (height* base) / 2;
+	}
+	
+	@Override
+	public float getPerimeter() {
+		return base + side1 + side2;
+	}
+}
