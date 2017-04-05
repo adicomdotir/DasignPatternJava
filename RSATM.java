989f9098eb387e0eb987dcffe65548c7a198672b
adicomdotir <adicom.ir@gmail.com>
2017-04-05 09:12:28 +0430

    Modify any file

--- RSATM.java
+++ RSATM.java
@@ -13,3 +13,33 @@ public class RSATM {
 		}
 	}  
 }
+
+class FlyingDragon extends Entity {
+private int Hp;
+public FlyingDragon(int x, int y, int Hp){
+    super (x, y);
+    this.Hp = Hp;
+}
+public void setHp(int Hp){
+    this.Hp = 100;
+}
+
+public int getHp(){
+   return Hp;}
+
+public void setType(String type) {
+ super.setType("Flying Dragon");}
+}
+
+    abstract class Entity { 
+private int x; // x coordinate in the room
+private int y; // y coordinate in the room
+
+
+ public Entity (int x, int y) {
+ this.x=x;
+ this.y =y;
+}
+public void setType(String type) {
+     }
+	}
