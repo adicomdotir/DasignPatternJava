297730b8d6276408e1b823da9956f4582804bebd
adicomdotir <adicom.ir@gmail.com>
2017-04-06 11:32:39 +0430

    Use TreeMap in code

--- RSATM.java
+++ RSATM.java
@@ -1,4 +1,5 @@
 import javax.swing.*;
+import java.util.TreeMap;
  
 public class RSATM {   
 	public static void main(String[] args) {
@@ -11,35 +12,49 @@ public class RSATM {
 		for(Shape s : shapes) {
 			System.out.println("" + s);
 		}
+		
+		TreeMap<String, String> hm = new TreeMap<String, String>();
+        //add key-value pair to TreeMap
+        hm.put("first", "FIRST INSERTED");
+        hm.put("second", "SECOND INSERTED");
+        hm.put("third","THIRD INSERTED");
+        System.out.println("My TreeMap content:");
+        System.out.println(hm);
+        System.out.println("Clearing TreeMap:");
+        hm.clear();
+        System.out.println("Content After clear:");
+        System.out.println(hm);
 	}  
 }
 
 class FlyingDragon extends Entity {
-private int Hp;
-public FlyingDragon(int x, int y, int Hp){
-    super (x, y);
-    this.Hp = Hp;
-}
-public void setHp(int Hp){
-    this.Hp = 100;
-}
-
-public int getHp(){
-   return Hp;}
-
-public void setType(String type) {
- super.setType("Flying Dragon");}
-}
-
-    abstract class Entity { 
-private int x; // x coordinate in the room
-private int y; // y coordinate in the room
-
-
- public Entity (int x, int y) {
- this.x=x;
- this.y =y;
-}
-public void setType(String type) {
-     }
+	private int Hp;
+	public FlyingDragon(int x, int y, int Hp) {
+		super (x, y);
+		this.Hp = Hp;
 	}
+	public void setHp(int Hp) {
+		this.Hp = 100;
+	}
+
+	public int getHp() {
+		return Hp;
+	}
+
+	public void setType(String type) {
+		super.setType("Flying Dragon");
+	}
+}
+
+abstract class Entity { 
+	private int x; // x coordinate in the room
+	private int y; // y coordinate in the room
+
+	public Entity (int x, int y) {
+		this.x=x;
+		this.y =y;
+	}
+	
+	public void setType(String type) {
+	}
+}
