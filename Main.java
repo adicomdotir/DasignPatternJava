1d7e66f854b1fa6842298c0dd9f0bb9dc7ad530b
adicomdotir <adicom.ir@gmail.com>
2017-04-22 09:33:40 +0430

    Add factory design pattern classes

--- Main.java
+++ Main.java
@@ -5,5 +5,10 @@ public class Main {
 		Student p2 = new Student();
 		p2.setGrade(15.2);
 		System.out.println(p1.equals(p2));
+		
+		Computer pc = ComputerFactory.getComputer("pc","2 GB","500 GB","2.4 GHz");
+		Computer server = ComputerFactory.getComputer("server","16 GB","1 TB","2.9 GHz");
+		System.out.println("Factory PC Config::" + pc);
+		System.out.println("Factory Server Config::" + server);
 	}
 }
