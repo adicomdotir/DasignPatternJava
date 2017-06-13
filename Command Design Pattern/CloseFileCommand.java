7d55c46fd80cfd3dec7db11cf4879e2e70e857ed
adicomdotir <adicom.ir@gmail.com>
2017-06-13 09:36:34 +0430

    Modify first Command DP

---
+++ Command Design Pattern/CloseFileCommand.java
@@ -0,0 +1,10 @@
+public class CloseFileCommand implements Command {
+	private FileSystemReceiver fileSystem;
+	public CloseFileCommand(FileSystemReceiver fs){
+		this.fileSystem=fs;
+	}
+	@Override
+	public void execute() {
+		this.fileSystem.closeFile();
+	}
+}
