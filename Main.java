f30781d3fe39801d81deadd98a4e720cd4e90821
Adicom <adicom.ir@gmail.com>
2017-07-06 00:45:16 +0430

    Add stringClean() method

--- Main.java
+++ Main.java
@@ -193,6 +193,21 @@ public class Main {
 		if(str.charAt(0)=='1' && str.charAt(1)=='1') return 1 + count11(str.substring(2));
 		else return count11(str.substring(1));
 	}
+	
+	/*
+	Given a string, return recursively a "cleaned" string where adjacent
+	chars that are the same have been reduced to a single char. 
+	So "yyzzza" yields "yza".
+
+	stringClean("yyzzza") → "yza"
+	stringClean("abbbcdd") → "abcd"
+	stringClean("Hello") → "Helo" 
+	*/
+	public String stringClean(String str) {
+		if(str.length() < 2) return str;
+		if(str.charAt(0)==str.charAt(1)) return stringClean(str.substring(1));
+		else return str.substring(0,1) + stringClean(str.substring(1));
+	}
 
 }
 
