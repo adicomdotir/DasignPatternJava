1cf7d75df750b4ce458be83a22dc39b3f2a50ba6
adicomdotir <adicom.ir@gmail.com>
2017-07-05 14:17:35 +0430

    Add countPair() method

--- Main.java
+++ Main.java
@@ -147,6 +147,24 @@ public class Main {
 		if(str.charAt(0) == 'x') return endX(str.substring(1)) + 'x';
 		else return str.substring(0,1) + endX(str.substring(1));
 	}
+	
+	/*
+	We'll say that a "pair" in a string is two instances of a char separated 
+	by a char. So "AxA" the A's make a pair. Pair's can overlap, so "AxAxA" 
+	contains 3 pairs -- 2 for A and 1 for x. Recursively compute the number 
+	of pairs in the given string.
+
+	countPairs("axa") → 1
+	countPairs("axax") → 2
+	countPairs("axbx") → 1
+	*/
+	public int countPairs(String str) {
+		if (str.length() < 3) return 0;
+		if (str.charAt(0) == str.charAt(2))
+			return 1 + countPairs(str.substring(1));
+		return countPairs(str.substring(1));
+	}
+
 }
 
 class CountAndPrint implements Runnable {
