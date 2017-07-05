b2dd17db84fd8fab167e4a6dcf5506fdddaa626f
adicomdotir <adicom.ir@gmail.com>
2017-07-05 11:53:01 +0430

    Add endX() method

--- Main.java
+++ Main.java
@@ -133,6 +133,20 @@ public class Main {
 		if(str.charAt(0) == str.charAt(1)) return str.charAt(0) + "*" + pairStar(str.substring(1));
 		else return str.substring(0, 1) + pairStar(str.substring(1));
 	}
+	
+	/*
+	Given a string, compute recursively a new string where all the lowercase 'x' 
+	chars have been moved to the end of the string.
+
+	endX("xxre") → "rexx"
+	endX("xxhixx") → "hixxxx"
+	endX("xhixhix") → "hihixxx"
+	*/
+	public String endX(String str) {
+		if(str.length() <= 1) return str;
+		if(str.charAt(0) == 'x') return endX(str.substring(1)) + 'x';
+		else return str.substring(0,1) + endX(str.substring(1));
+	}
 }
 
 class CountAndPrint implements Runnable {
