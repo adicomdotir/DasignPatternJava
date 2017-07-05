95e3bacb90fb0d0ad38126546fd7420045399d53
Adicom <adicom.ir@gmail.com>
2017-07-05 19:40:34 +0430

    Add count11() method

--- Main.java
+++ Main.java
@@ -179,6 +179,20 @@ public class Main {
 		if(str.charAt(0) == 'a' && str.charAt(1) == 'b' && str.charAt(2) == 'a') return 1 + countAbc(str.substring(3));
 		return countAbc(str.substring(1));
 	}
+	
+	/*
+	Given a string, compute recursively (no loops) the number of "11" 
+	substrings in the string. The "11" substrings should not overlap.
+
+	count11("11abc11") → 2
+	count11("abc11x11x11") → 3
+	count11("111") → 1
+	*/
+	public int count11(String str) {
+		if(str.length() < 2) return 0;
+		if(str.charAt(0)=='1' && str.charAt(1)=='1') return 1 + count11(str.substring(2));
+		else return count11(str.substring(1));
+	}
 
 }
 
