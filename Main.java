e68c63bb2146e3ac838ba7f73d85585b69e385d9
adicomdotir <adicom.ir@gmail.com>
2017-07-06 09:31:48 +0430

    Add parenBit() method

--- Main.java
+++ Main.java
@@ -224,6 +224,23 @@ public class Main {
 		if(str.charAt(0) == 'x' && str.charAt(1) == 'h' && str.charAt(2) == 'i') return countHi2(str.substring(2));
 		else return countHi2(str.substring(1));
 	}
+	
+	/*
+	Given a string that contains a single pair of parenthesis, compute
+	recursively a new string made of only of the parenthesis and their
+	contents, so "xyz(abc)123" yields "(abc)".
+
+	parenBit("xyz(abc)123") → "(abc)"
+	parenBit("x(hello)") → "(hello)"
+	parenBit("(xy)1") → "(xy)"
+	*/
+	public String parenBit(String str) {
+		int len = str.length();
+		if(len < 2) return "";
+		if(str.charAt(0) == '(' && str.charAt(len-1) == ')') return str;
+		if(str.charAt(0) == '(' && str.charAt(len-1) != ')') return parenBit(str.substring(0,len-1));
+		else return parenBit(str.substring(1));
+	}
 }
 
 class CountAndPrint implements Runnable {
