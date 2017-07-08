f70c82039ff940b0bc474a3d625b04e370ebcee7
adicomdotir <adicom.ir@gmail.com>
2017-07-08 14:35:48 +0430

    Add strCount() method

--- Main.java
+++ Main.java
@@ -241,6 +241,23 @@ public class Main {
 		if(str.charAt(0) == '(' && str.charAt(len-1) != ')') return parenBit(str.substring(0,len-1));
 		else return parenBit(str.substring(1));
 	}
+	
+	/*
+	Given a string and a non-empty substring sub, compute recursively 
+	the number of times that sub appears in the string, without the 
+	sub strings overlapping.
+
+	strCount("catcowcat", "cat") → 2
+	strCount("catcowcat", "cow") → 1
+	strCount("catcowcat", "dog") → 0
+	*/
+	public int strCount(String str, String sub) {
+		int len = sub.length();
+		if(str.length() < len) return 0;
+		if(str.substring(0,len).equals(sub)) return 1 + strCount(str.substring(len), sub);
+		else return strCount(str.substring(1), sub);
+	}	
+
 }
 
 class CountAndPrint implements Runnable {
