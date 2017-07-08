c69301a8a3f3cc904f3c9fbff83e164439e09d65
Adicom <adicom.ir@gmail.com>
2017-07-08 23:03:20 +0430

    Add strCopies() method

--- Main.java
+++ Main.java
@@ -274,6 +274,19 @@ public class Main {
 		if(str.substring(0,len).equals(sub)) return 1 + strCount(str.substring(len), sub);
 		else return strCount(str.substring(1), sub);
 	}
+
+	/*
+	Given a string and a non-empty substring sub, compute recursively 
+	if at least n copies of sub appear in the string somewhere, 
+	possibly with overlapping. N will be non-negative.
+
+	strCopies("catcowcat", "cat", 2) → true
+	strCopies("catcowcat", "cow", 2) → false
+	strCopies("catcowcat", "cow", 1) → true
+	*/
+	public boolean strCopies(String str, String sub, int n) {
+		  
+	}
 }
 
 class CountAndPrint implements Runnable {
