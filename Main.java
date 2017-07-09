3f0dab51e8cc608fca77c17b3760310f373fcf6f
adicomdotir <adicom.ir@gmail.com>
2017-07-09 11:41:11 +0430

    pairs

--- Main.java
+++ Main.java
@@ -347,6 +347,23 @@ public class Main {
 		}
 		return map;
 	}
+	
+	/*
+	Given an array of non-empty strings, create and return a 
+	Map<String, String> as follows: for each string add its first 
+	character as a key with its last character as the value.
+
+	pairs(["code", "bug"]) → {"b": "g", "c": "e"}
+	pairs(["man", "moon", "main"]) → {"m": "n"}
+	pairs(["man", "moon", "good", "night"]) → {"g": "d", "m": "n", "n": "t"}
+	*/
+	public Map<String, String> pairs(String[] strings) {
+		Map<String, Integer> map = new HashMap<>();
+		for(String str: strings) {
+			map.put(str.substring(0,1), str.substring(str.length()-1));
+		}
+		return map;
+	}
 }
 
 class CountAndPrint implements Runnable {
