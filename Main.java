7a44b8dd8fe6c015e9f28ab7caa90f8baa4aed57
adicomdotir <adicom.ir@gmail.com>
2017-07-11 16:09:19 +0430

    Add noNeg method

--- Main.java
+++ Main.java
@@ -453,7 +453,49 @@ public class Main {
 			} else	map.put(key, false);
 		}
 		return map;
-	}	
+	}
+	
+	/*
+	list.replaceAll(lambda) -- calls the lambda once for each item in the 
+	list, storing the result back into the list (or other type of collection). 
+	e.g. nums.replaceAll(n -> n * 2);
+
+	list.removeIf(lambda) -- calls the lambda once for each item in the 
+	collection, removing each item where the lambda returns true. 
+	e.g. nums.removeIf(n -> n < 0);
+
+	Simple Lambda Examples -- the data types are inferred from the context 
+	and from the type of the expression following the "->": 
+	n -> n * 2 -- takes Integer, returns Integer 
+	n -> n < 0 && n >= -10 -- takes Integer, returns boolean 
+	s -> s.length()  -- takes String, returns Integer 
+	s -> s.startsWith("hi") -- takes String, returns boolean
+
+	The Java stream system provides more complicated lambda features. 
+	The stream calls do not modify the original list, returning a new data 
+	structure of the results. Note that the boolean logic of filter() is 
+	the opposite of removeIf().
+
+	  List<Integer> nums = -something-;
+	  nums = nums.stream()
+		.map(n -> n * 2)
+		.filter(n -> n >= 0)
+		.collect(Collectors.toList());
+	*/
+	
+	/*
+	Given a list of integers, return a list of the integers, omitting any 
+	that are less than 0.
+
+	noNeg([1, -2]) → [1]
+	noNeg([-3, -3, 3, 3]) → [3, 3]
+	noNeg([-1, -1, -1]) → []
+	*/
+	public List<Integer> noNeg(List<Integer> nums) {
+		return nums.stream()
+		.filter(n -> n>=0)
+		.collect(Collectors.toList());
+	}
 }
 
 class CountAndPrint implements Runnable {
