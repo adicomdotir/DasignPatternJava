e20eb299f72633eafbdeb90c5b47857b214753e4
adicomdotir <adicom.ir@gmail.com>
2017-07-13 09:12:14 +0430

    Add noLong method

--- Main.java
+++ Main.java
@@ -524,6 +524,21 @@ public class Main {
 		.filter(n -> n<13 || n>19)
 		.collect(Collectors.toList());
 	}
+	
+	/*
+	Given a list of strings, return a list of the strings, omitting 
+	any string length 4 or more.
+
+	noLong(["this", "not", "too", "long"]) → ["not", "too"]
+	noLong(["a", "bbb", "cccc"]) → ["a", "bbb"]
+	noLong(["cccc", "cccc", "cccc"]) → []
+	*/
+	public List<String> noLong(List<String> strings) {
+		return strings.stream()
+		.filter(s -> s.length()<4)
+		.collect(Collectors.toList());
+	}
+
 }
 
 class CountAndPrint implements Runnable {
