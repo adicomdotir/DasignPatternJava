13e3431f5c1f6810da47c179a7790b2f56ae4935
Adicom <adicom.ir@gmail.com>
2017-07-14 12:28:11 +0430

    Add no34() method

--- Main.java
+++ Main.java
@@ -554,6 +554,20 @@ public class Main {
 		.collect(Collectors.toList());
 	}
 
+	/*
+	Given a list of strings, return a list of the strings, 
+	omitting any string length 3 or 4.
+
+	no34(["a", "bb", "ccc"]) → ["a", "bb"]
+	no34(["a", "bb", "ccc", "dddd"]) → ["a", "bb"]
+	no34(["ccc", "dddd", "apple"]) → ["apple"]
+	*/
+	public List<String> no34(List<String> strings) {
+  		return strings.stream()
+		.filter(s -> s.length()!=3 && s.length()!=4)
+		.collect(Collectors.toList());
+	}
+
 }
 
 class CountAndPrint implements Runnable {
