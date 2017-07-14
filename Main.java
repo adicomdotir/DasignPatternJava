de83033195f56f0d62cefa0c946170a08ff935c0
Adicom <adicom.ir@gmail.com>
2017-07-14 12:34:08 +0430

    Add noYY() method

--- Main.java
+++ Main.java
@@ -568,6 +568,22 @@ public class Main {
 		.collect(Collectors.toList());
 	}
 
+	/*
+	Given a list of strings, return a list where each string 
+	has "y" added at its end, omitting any resulting strings 
+	that contain "yy" as a substring anywhere.
+
+	noYY(["a", "b", "c"]) → ["ay", "by", "cy"]
+	noYY(["a", "b", "cy"]) → ["ay", "by"]
+	noYY(["xx", "ya", "zz"]) → ["xxy", "yay", "zzy"]
+	*/
+	public List<String> noYY(List<String> strings) {
+  		return strings.stream()
+		.map(s -> s += "y")
+		.filter(s -> !s.contains("yy"))
+		.collect(Collectors.toList());
+	}
+
 }
 
 class CountAndPrint implements Runnable {
