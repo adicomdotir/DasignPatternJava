626adc43bbe0b671fa4e72498b301981c73802e3
Adicom <adicom.ir@gmail.com>
2017-07-14 12:25:42 +0430

    Add noZ() method

--- Main.java
+++ Main.java
@@ -538,6 +538,21 @@ public class Main {
 		.filter(s -> s.length()<4)
 		.collect(Collectors.toList());
 	}
+	
+	/*
+	Given a list of strings, return a list of the strings, 
+	omitting any string that contains a "z". 
+	(Note: the str.contains(x) method returns a boolean)
+
+	noZ(["aaa", "bbb", "aza"]) → ["aaa", "bbb"]
+	noZ(["hziz", "hzello", "hi"]) → ["hi"]
+	noZ(["hello", "howz", "are", "youz"]) → ["hello", "are"]
+	 */
+	 public List<String> noZ(List<String> strings) {
+  		return strings.stream()
+		.filter(s -> !s.contains("z"))
+		.collect(Collectors.toList());
+	}
 
 }
 
