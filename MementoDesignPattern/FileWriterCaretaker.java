63ad0ef3262a8b9bbbfe818295e031a1129a7d8e
adicomdotir <adicom.ir@gmail.com>
2017-08-01 09:10:41 +0430

    Modify Memento Design Pattern

--- MementoDesignPattern/FileWriterCaretaker.java
+++ MementoDesignPattern/FileWriterCaretaker.java
@@ -1,9 +1,9 @@
 public class FileWriterCaretaker {
-private Object obj;
-public void save(FileWriterUtil fileWriter){
-this.obj=fileWriter.save();
-}
-public void undo(FileWriterUtil fileWriter){
-fileWriter.undoToLastSave(obj);
-}
+	private Object obj;
+	public void save(FileWriterUtil fileWriter){
+		this.obj=fileWriter.save();
+	}
+	public void undo(FileWriterUtil fileWriter){
+		fileWriter.undoToLastSave(obj);
+	}
 }
