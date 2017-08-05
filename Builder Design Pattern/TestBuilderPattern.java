259cb1966dec116f629d30d9a0e0fb1227247334
adicomdotir <adicom.ir@gmail.com>
2017-08-05 09:20:54 +0430

    Delete files

--- Builder Design Pattern/TestBuilderPattern.java
+++
@@ -1,7 +0,0 @@
-public class TestBuilderPattern {
-	public static void main(String[] args) {
-		Computer comp = new Computer.ComputerBuilder("500 GB", "2 GB")
-			.setBluetoothEnabled(true)
-			.setGraphicsCardEnabled(true).build();
-	}
-}
