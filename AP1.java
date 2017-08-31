9ea6ee6c55d4f3eae49a19bfd37cd54b9281d678
adicomdotir <adicom.ir@gmail.com>
2017-08-31 09:43:36 +0430

    Add method wordsCount

--- AP1.java
+++ AP1.java
@@ -30,4 +30,22 @@ public class AP1 {
 		secondHalf /= half;
 		return firstHalf > secondHalf ? firstHalf : secondHalf;
 	}
+
+	/*
+	Given an array of strings, return the count of the number of 
+	strings with the given length.
+
+	wordsCount(["a", "bb", "b", "ccc"], 1) → 2
+	wordsCount(["a", "bb", "b", "ccc"], 3) → 1
+	wordsCount(["a", "bb", "b", "ccc"], 4) → 0
+	*/
+	public int wordsCount(String[] words, int len) {
+		int sum = 0;
+		for (int i=0; i<words.length; i++) {
+			if (words[i].length() == len) {
+				sum++;
+			}
+		}
+		return sum;
+	}
 }
