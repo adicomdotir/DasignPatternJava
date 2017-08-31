0a259562297f08c8317485e37dd1ea5e65748248
Adicom <adicom.ir@gmail.com>
2017-08-31 20:38:28 +0430

    Add method copyEndy

--- AP1.java
+++ AP1.java
@@ -126,4 +126,28 @@ public class AP1 {
 		}
 		return numbers;
 	}
+
+	/*
+	We'll say that a positive int n is "endy" if it is in the range 
+	0..10 or 90..100 (inclusive). Given an array of positive ints, 
+	return a new array of length "count" containing the first endy 
+	numbers from the original array. Decompose out a separate isEndy(int n) 
+	method to test if a number is endy. The original array will contain 
+	at least "count" endy numbers.
+
+	copyEndy([9, 11, 90, 22, 6], 2) → [9, 90]
+	copyEndy([9, 11, 90, 22, 6], 3) → [9, 90, 6]
+	copyEndy([12, 1, 1, 13, 0, 20], 2) → [1, 1]
+	*/
+	public int[] copyEndy(int[] nums, int count) {
+		int[] numbers = new int[count];
+		for (int i = 0, j = 0; i < count; j++) {
+			if ((nums[j] >= 0 && nums[j] <= 10) || (nums[j] >= 90 && nums[j] <= 100)) {
+				numbers[i] = nums[j];
+				i++;
+			}			
+		}
+		return numbers;
+	}
+
 }
