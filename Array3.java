64d726d18bb3cb870a78c24ac4b764f910e620f7
Adicom <adicom.ir@gmail.com>
2017-09-09 11:15:59 +0430

    Add method countClumps

--- Array3.java
+++ Array3.java
@@ -69,4 +69,26 @@ public class Array3 {
 		}
 		return max;
 	}
+	
+	/*
+	Say that a "clump" in an array is a series of 2 or more adjacent elements of 
+	the same value. Return the number of clumps in the given array.
+
+	countClumps([1, 2, 2, 3, 4, 4]) → 2
+	countClumps([1, 1, 2, 1, 1]) → 2
+	countClumps([1, 1, 1, 1, 1]) → 1
+	*/
+	public int countClumps(int[] nums) {
+		boolean match = false;
+		int count = 0;
+		for (int i = 0; i < nums.length - 1; i++) {
+			if (nums[i] == nums[i+1] && !match) {
+				match = true;
+				count++;
+			} else if (nums[i] != nums[i+1]) {
+				match = false;
+			}
+		}
+		return count;
+	}
 }
