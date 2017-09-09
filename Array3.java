7fc4220b3b6de7b75bc7716f232ddc77505e2593
Adicom <adicom.ir@gmail.com>
2017-09-09 10:01:17 +0430

    Add method seriesUp

--- Array3.java
+++ Array3.java
@@ -23,4 +23,25 @@ public class Array3 {
 		}
 		return result;
 	}
+	
+	/*
+	Given n>=0, create an array with the pattern 
+	{1,    1, 2,    1, 2, 3,   ... 1, 2, 3 .. n} 
+	(spaces added to show the grouping). Note that the length of 
+	the array will be 1 + 2 + 3 ... + n, which is known to sum to 
+	exactly n*(n + 1)/2.
+
+	seriesUp(3) → [1, 1, 2, 1, 2, 3]
+	seriesUp(4) → [1, 1, 2, 1, 2, 3, 1, 2, 3, 4]
+	seriesUp(2) → [1, 1, 2]
+	*/
+	public int[] seriesUp(int n) {
+		int[] arr = new int[n*(n+1)/2];
+		for (int i=1; i<=n; i++) {
+			for (int j=1, k=i*(i+1)/2-i; j<=i; j++,k++) {
+				arr[k] = j;
+			}
+		}
+		return arr;
+	}
 }
