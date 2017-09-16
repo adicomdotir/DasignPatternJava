902bc617ce5455f64f9e5b1077fd47d06f5cab5f
adicomdotir <adicom.ir@gmail.com>
2017-09-16 14:51:49 +0430

    Add method split53

--- Recursion2.java
+++ Recursion2.java
@@ -155,4 +155,29 @@ public class Recursion2 {
 			return true;
 		return sidesAreEqual10(nums, i + 1, sum1, sum2 + nums[i]);
 	}
+
+	/*
+	Given an array of ints, is it possible to divide the ints into two groups, 
+	so that the sum of the two groups is the same, with these constraints: 
+	all the values that are multiple of 5 must be in one group, and all the 
+	values that are a multiple of 3 (and not a multiple of 5) must be in the other. 
+	(No loops needed.)
+
+	split53([1, 1]) → true
+	split53([1, 1, 1]) → false
+	split53([2, 4, 2]) → true
+	*/
+	public boolean split53(int[] nums) {
+		return sidesAreEqual53(nums, 0, 0, 0);
+	}
+
+	public boolean sidesAreEqual53(int[] nums, int i, int sum1, int sum2) {
+		if(i == nums.length)
+			return (sum1 == sum2);
+		if(nums[i] % 3 != 0 && sidesAreEqual53(nums, i + 1, sum1 + nums[i], sum2))
+			return true;
+		if(nums[i] % 5 != 0 && sidesAreEqual53(nums, i + 1, sum1, sum2 + nums[i]))
+			return true;
+		else return false;
+	}
 }
