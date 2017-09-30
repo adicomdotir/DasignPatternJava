5da08f5ab1ba59823e0b7418c6c8d00574fce357
Adicom <adicom.ir@gmail.com>
2017-09-30 11:50:00 +0330

    Add index.html to LeagueTable

--- LeagueTable/round_robin_tour.js
+++ LeagueTable/round_robin_tour.js
@@ -1,7 +1,4 @@
-var size = 4;
-var teams = [0, 1, 2, 3];
-
-function Team(name) {
+function Team(name, overall) {
 	this.name = name;
 	this.game = 0;
 	this.win = 0;
@@ -11,14 +8,44 @@ function Team(name) {
 	this.ga = 0;
 	this.gd = 0;
 	this.points = 0;
+	this.overall = overall;
 }
 
+var size = 18;
+var teams = [];
+for (var i = 0; i < size; i++) {
+	teams[i] = i;
+}
 var teamsInfo = [];
-var barca = new Team('Barcelona');
-var rm = new Team('RealMadrid');
-var psg = new Team('PSG');
-var chelsea = new Team('Chelsea');
-teamsInfo.push(barca, rm, psg, chelsea);
+var t1 = new Team('SL Benfica', 79);
+var t2 = new Team('FC Porto', 79);
+var t3 = new Team('SC Braga', 75);
+var t4 = new Team('Sporting CP', 78);
+var t5 = new Team('V. Guimarães', 74);
+var t6 = new Team('V. Setúbal', 72);
+var t7 = new Team('Arouca', 71);
+var t8 = new Team('Ave	Rio Ave', 72);
+var t9 = new Team('Chaves', 71);
+var t10 = new Team('Paços Ferreira', 71);
+var t11 = new Team('Boavista', 71);
+var t12 = new Team('Marítimo', 71);
+var t13 = new Team('Belenenses', 70);
+var t14 = new Team('Estoril Praia', 69);
+var t15 = new Team('Moreirense', 69);
+var t16 = new Team('F. Santa Maria', 69);
+var t17 = new Team('Nacional', 69);
+var t18 = new Team('Tondela', 68);
+teamsInfo.push(t1, t2, t3, t4, t5, t6,
+				t7, t8, t9, t10, t11, t12,
+				t13, t14, t15, t16, t17, t18);
+// var teamsInfo = [];
+// var barca = new Team('Barcelona', 85);
+// var rm = new Team('RealMadrid', 86);
+// var psg = new Team('PSG', 83);
+// var chelsea = new Team('Chelsea', 83);
+// var inter = new Team('Inter', 81);
+// var milan = new Team('Milan', 79);
+// teamsInfo.push(barca, rm, psg, chelsea, inter, milan);
 
 function swap() {
 	var temp = teams[size - 1];
@@ -28,14 +55,15 @@ function swap() {
 	teams[1] = temp;
 }
 
-for (var i = 1; i < size; i++) {
+for (var i = 1; i < size * 2 - 1; i++) {
 	var str = '';
-  var header = document.createElement("h3");
-        header.appendChild(document.createTextNode("Week" + i));
-  document.getElementById('figure').appendChild(header);
+  	var header = document.createElement("b");
+    header.appendChild(document.createTextNode("Week" + i));
+	document.getElementById('figure').appendChild(header);
 	for (var j = 0; j < size / 2; j++) {
-		var gA = Math.floor((Math.random() * 5));
-		var gB = Math.floor((Math.random() * 5));
+		var gA = Math.floor(((Math.random() * 5) + (teamsInfo[teams[j]].overall / 20)) / 2);
+		var gB = Math.floor(((Math.random() * 5) + (teamsInfo[teams[size - j - 1]].overall / 20)) / 2);
+		console.log(gA);
 		if (gA === gB) {
 			teamsInfo[teams[j]].game++;
 			teamsInfo[teams[j]].draw++;
@@ -73,22 +101,23 @@ for (var i = 1; i < size; i++) {
         div.appendChild(document.createTextNode(str));
         document.getElementById('figure').appendChild(div);
 	}
-    
+
 	// console.log(str);
     swap();
 }
 sort();
-//console.log(teamsInfo);
+// console.log(teamsInfo);
 for (var i = 0; i < size; i++) {
 	var row = document.createElement("tr");
 	for (var key in teamsInfo[i]) {
 		var col = document.createElement("td");
 		if (key === 'gd') {
 			col.appendChild(document.createTextNode(teamsInfo[i]['gf'] - teamsInfo[i]['ga']));
-		} else {
+			row.appendChild(col);
+		} else if (key !== 'overall') {
 			col.appendChild(document.createTextNode(teamsInfo[i][key]));
+			row.appendChild(col);
 		}
-		row.appendChild(col);
 	}
 	document.getElementById('tbody').appendChild(row);
 }
@@ -100,7 +129,7 @@ function sort() {
 				var temp = teamsInfo[j];
 				teamsInfo[j] = teamsInfo[i];
 				teamsInfo[i] = temp;
-			} 
+			}
 		}
 	}
 	for (var i = 0; i < size; i++) {
