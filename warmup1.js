435ebe990a733f1026602ed65d421cae1f496495
Adicom <adicom.ir@gmail.com>
2017-10-12 22:09:47 +0330

    Add diff21 method

--- warmup1.js
+++ warmup1.js
@@ -1,3 +1,8 @@
-nearHundred = function(n){ 
+nearHundred = function(n) { 
 	return (Math.abs(100 - n) <= 10) || (Math.abs(200 - n) <= 10);
+}
+
+var diff21 = function(n) { 
+	if (n < 21) return 21 - n;
+  	else return (n - 21) * 2;
 } 
