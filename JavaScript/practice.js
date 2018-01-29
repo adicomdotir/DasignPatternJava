5bfa23fc2d353afe9e7be2f6be049fe8207d1176
adicomdotir <adicom.ir@gmail.com>
2018-01-29 15:48:48 +0330

    closure added js

--- JavaScript/practice.js
+++ JavaScript/practice.js
@@ -1,21 +1,37 @@
 function User(){
 	var username, password;
-
 	function doLogin(user,pw) {
 		username = user;
 		password = pw;
 
 		// do the rest of the login work
 	}
-
 	var publicAPI = {
 		login: doLogin
 	};
-
 	return publicAPI;
 }
 
 // create a `User` module instance
 var fred = User();
-
 fred.login( "adicom", "12Battery34!" );
+
+//--------------------//
+(function IIFE(){
+	console.log( "Hello!" );
+})();
+// "Hello!"
+
+function makeNum(x) {
+	function add(y) {
+		return y + x;
+	};
+	return add;
+}
+
+var addOne = makeNum(1);
+var addTen = makeNum(10);
+
+addOne(3);		// 4  <-- 1 + 3
+addOne(41);		// 42 <-- 1 + 41
+addTen(13);		// 23 <-- 10 + 13
