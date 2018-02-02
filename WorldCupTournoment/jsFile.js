57e2095f2007c7c37d572eb797cb738c16d2a958
Adicom.ir <adicom.ir@gmail.com>
2018-02-02 15:26:26 +0330

    Add new db structure wc

--- WorldCupTournoment/jsFile.js
+++ WorldCupTournoment/jsFile.js
@@ -825,6 +825,23 @@ function treeView() {
 }
 
 function db(champion, runner) {
+    //Create the database
+    var db = new SQL.Database();
+	// Run a query without reading the results
+    db.run("CREATE TABLE test (col1, col2);");
+    // Insert two rows: (1,111) and (2,222)
+    db.run("INSERT INTO test VALUES (?,?), (?,?)", [1,111,2,222]);
+
+	// Prepare a statement
+    var stmt = db.prepare("SELECT * FROM test WHERE col1 BETWEEN $start AND $end");
+    stmt.getAsObject({$start:1, $end:1}); // {col1:1, col2:111}
+
+    // Bind new values
+    stmt.bind({$start:1, $end:2});
+    while(stmt.step()) { //
+        var row = stmt.getAsObject();
+		console.log(row);
+    }
 	//prefixes of implementation that we want to test
 	var indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 	var IDBKeyRange = window.IDBKeyRange || window.webkitIDBKeyRange;
