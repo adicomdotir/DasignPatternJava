c6699368af3f2b80eb8bf21678d7f7348501f273
adicomdotir <adicom.ir@gmail.com>
2018-06-11 09:16:35 +0430

    Add string compare example

---
+++ NoDesignPattern/Example.java
@@ -0,0 +1,9 @@
+public class Example {
+	public static void main(String[] args) {
+		String str1 = new String(new char[] {'a', 'b', 'c'});
+		String str2 = "abc";
+		System.out.println(str1 == str2);
+		System.out.println(str1.equals(str2));
+		System.out.println(str1.compareTo(str2));
+	}
+}
