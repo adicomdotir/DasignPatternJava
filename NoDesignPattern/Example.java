2daf87a08060fde516a43688fed6f9345e9b375b
adicomdotir <adicom.ir@gmail.com>
2018-06-11 12:52:42 +0430

    Format code

--- NoDesignPattern/Example.java
+++ NoDesignPattern/Example.java
@@ -1,5 +1,9 @@
 public class Example {
 	public static void main(String[] args) {
+		stringExample();
+	}
+
+	public static String stringExample() {
 		String str1 = new String(new char[] {'a', 'b', 'c'});
 		String str2 = "abc";
 		System.out.println(str1 == str2);
