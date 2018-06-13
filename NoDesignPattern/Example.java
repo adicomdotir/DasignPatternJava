b588e976c25bcb2748954abd4afb202b50e6ebde
adicomdotir <adicom.ir@gmail.com>
2018-06-13 12:34:39 +0430

    Add method referece example

--- NoDesignPattern/Example.java
+++ NoDesignPattern/Example.java
@@ -3,11 +3,24 @@ public class Example {
 		stringExample();
 	}
 
-	public static String stringExample() {
+	public static void stringExample() {
 		String str1 = new String(new char[] {'a', 'b', 'c'});
 		String str2 = "abc";
 		System.out.println(str1 == str2);
 		System.out.println(str1.equals(str2));
 		System.out.println(str1.compareTo(str2));
 	}
+
+	public static void methodReferenceJava8() {
+		Sayable sayable = Example::printFullName;
+		sayable.say();
+	}
+
+	public static void printFullName() {
+		System.out.println("adicomdotir");
+	}
+}
+
+interface Sayable {
+	void say();
 }
