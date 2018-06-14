cb5e88b2af04bfc2d333f407a9d51d46f9c52586
adicomdotir <adicom.ir@gmail.com>
2018-06-14 12:30:52 +0430

    Add stream feature of java8

--- NoDesignPattern/Example.java
+++ NoDesignPattern/Example.java
@@ -1,26 +1,35 @@
+import java.util.Arrays;
+import java.util.stream.Stream;
+
 public class Example {
-	public static void main(String[] args) {
-		stringExample();
-	}
+    public static void main(String[] args) {
+        streamExample();
+    }
 
-	public static void stringExample() {
-		String str1 = new String(new char[] {'a', 'b', 'c'});
-		String str2 = "abc";
-		System.out.println(str1 == str2);
-		System.out.println(str1.equals(str2));
-		System.out.println(str1.compareTo(str2));
-	}
+    public static void stringExample() {
+        String str1 = new String(new char[] {'a', 'b', 'c'});
+        String str2 = "abc";
+        System.out.println(str1 == str2);
+        System.out.println(str1.equals(str2));
+        System.out.println(str1.compareTo(str2));
+    }
 
-	public static void methodReferenceJava8() {
-		Sayable sayable = Example::printFullName;
-		sayable.say();
-	}
+    public static void streamExample() {
+        String[] names = new String[] {"A", "Bb", "Ccc", "Dddd", "Eeeee"};
+        Stream<String> stream = Arrays.stream(names);
+        stream.filter(x -> x.length() > 2).forEach(System.out::println);
+    }
 
-	public static void printFullName() {
-		System.out.println("adicomdotir");
-	}
+    public static void methodReferenceJava8() {
+        Sayable sayable = Example::printFullName;
+        sayable.say();
+    }
+
+    public static void printFullName() {
+        System.out.println("adicomdotir");
+    }
 }
 
 interface Sayable {
-	void say();
+    void say();
 }
