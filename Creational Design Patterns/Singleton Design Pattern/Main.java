027a5e1ec483e57e1dae4d7ceb3d432eb52d8a80
Adicom.ir <adicom.ir@gmail.com>
2018-07-06 11:36:40 +0430

    Modify singleton dp

--- Creational Design Patterns/Singleton Design Pattern/Main.java
+++ Creational Design Patterns/Singleton Design Pattern/Main.java
@@ -1,6 +1,13 @@
-class Main {
+public class Main {
 	public static void main(String[] args) {
 		Singleton01 sing = Singleton01.getInstance();
-		
+		Student st = new Student();
+		st.show();
 	}
+}
+
+class Person {
+}
+
+class Student extends Person {
 }
